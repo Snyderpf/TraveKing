7f2b87ca31e983070b4c3cdaf6c9fdee3f3a36fa
snyder petit <snyderpetitfrere@gmail.com>
2021-09-30 16:26:55 -0400

    updating travel urls

--- Travel/Travel/urls.py
+++ Travel/Travel/urls.py
@@ -19,8 +19,8 @@ from django.urls import conf, path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from . import views
 urlpatterns = [
    
     path('admin/', admin.site.urls),
+    path('', include('WaveFlights.url'))
 ]+ static(settings.STATIC_URL,document_root =settings.STATIC_ROOT)
