c088a98431e5ac0033af5a5ba5b78a0787815fe3
snyder petit <snyderpetitfrere@gmail.com>
2021-09-30 16:26:12 -0400

    new app WaveFlights

---
+++ Travel/WaveFlights/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def blog(request):
+    return render (request, 'blog.html')
+
+def car(request):
+    return render (request, 'car.html')
+
+def contact(request):
+    return render (request, ' contact.html')
+
+def flight(request):
+    return render (request, 'flight.html')
+
+def hotel(request):
+    return render('hotel.h')
+
+def index(request):
+    return render (request, 'index.html')
+
+def vacation(request):
+    return render (request, 'vacation.html')
+
